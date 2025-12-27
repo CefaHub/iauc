f1ab13862eb56a06d84738eec53fe0b052beac33
CefaHub <112974480+CefaHub@users.noreply.github.com>
2025-12-27 16:22:44 +0300

    Delete src/main directory

--- src/main/java/ru/illit/auction/ViewContext.java
+++
@@ -1,8 +0,0 @@
-package ru.illit.auction;
-
-public final class ViewContext {
-    public int page = 0;
-    public AuctionService.Sort sort = AuctionService.Sort.NEW_FIRST;
-    public String search = "";
-    public ViewContext() {}
-}
